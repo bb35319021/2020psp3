60ce00a587bf9eee9718ff72d97c30960c89461b
bb35319021 <0510satomaru@gmail.com>
2021-01-24 23:46:54 +0900

    k05提出

--- k05/k05.c
+++ k05/k05.c
@@ -106,7 +106,32 @@ int StackIsEmpty(void)
 void DepthFirstSearch(int size, int matrix[size][size], int start)
 {
     //  ここを実装する
+int index, i, visited[size];
 
+for(index=0;index<size;index++){
+    visited[index]=0;
+}
+
+StackInit();
+StackPush(start);
+
+while(StackIsEmpty()==FALSE){
+    index=StackPop();
+    i=0;
+
+    if(visited[index]==0){
+        visited[index]=1;
+
+        PrintStationName(index);
+
+        while(i<size){
+            if(matrix[index][i]!=0){
+                StackPush(i);
+            }
+            i++;
+        }
+    }
+}
 }
 
 
@@ -172,7 +197,31 @@ int QueueIsEmpty()
 void BreadthFirstSearch(int size, int matrix[size][size], int start)
 {
     //  ここを実装する
+int index,i,visited[size];
 
+for(index=0;index<size;index++){
+    visited[index]=0;
+}
+
+InitQueue();
+EnQueue(start);
+
+while(QueueIsEmpty()==FALSE){
+    index=DeQueue();
+    i=0;
+
+    if(visited[index]==0){
+        visited[index]=1;
+
+        PrintStationName(index);
+        while(i<size){
+            if(matrix[index][i]!=0){
+                EnQueue(i);
+            }
+            i++;
+        }
+    }
+}
 }
 
 
